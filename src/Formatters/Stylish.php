61e9e5bf3f7eb960686a27f2c8a97e8f1dabf888
root <root@Maxim.localdomain>
2026-03-09 19:51:10 +0300

    Fix lint#2

--- src/Formatters/Stylish.php
+++ src/Formatters/Stylish.php
@@ -4,7 +4,7 @@ namespace Hexlet\Code\Formatters;
 
 class Stylish
 {
-    private const int INDENT_SIZE = 4;
+    private const INDENT_SIZE = 4;
 
     /**
      * Форматирует AST дерево в stylish формат
